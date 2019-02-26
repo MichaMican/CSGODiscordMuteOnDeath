bb73f0cb69c5f8f1d8a306f6499bc7ea0d20456b
Michael Mican <michael.mican@stud.th-deg.de>
2019-02-26 21:36:57 +0100

    Add key listener and (un)muting function

--- HTTPServer/Server.py
+++ HTTPServer/Server.py
@@ -5,6 +5,8 @@ from flask import Flask, request, render_template
 from flask_cors import CORS
 import json
 from threading import Thread, Lock
+from pynput import keyboard
+import os
 
 
 app = Flask(__name__)
@@ -14,6 +16,9 @@ game_data = "./IO/game_data.json"
 
 idx = 0
 deaths = 0
+gameRound = 0
+isMuted = False
+pause = False
 
 @app.route("/read", methods=['GET'])
 def read_data():
@@ -43,20 +48,97 @@ def index():
 
 def analyse(data):
     global deaths
+    global gameRound
+    global isMuted
+    
     currentDeaths = deaths
+    currentRound = gameRound
     try:
         currentDeaths = data["player"]["match_stats"]["deaths"]
+        currentRound = data["map"]["round"]
     except:
-        print("Player is in no match")
+        print("Player is not in a match")
         deaths = 0
         currentDeaths = 0
-    
-    if currentDeaths > deaths:
-        deaths = data["player"]["match_stats"]["deaths"]
+        currentRound = 0
+        gameRound = 0
+
+    #This part mutes the user
+    if currentDeaths > deaths and not isMuted and not pause:
+        deaths = currentDeaths
+        isMuted = True
+        mute()
         print("MUTED!")
 
+    #This part unmutes the user
+    if currentRound > gameRound and isMuted and not pause:
+        isMuted = False
+        gameRound = currentRound
+        unMute()
+        print("UNMUTED!")
+
+
+def on_press(key):
+    global pause
+    global deaths
+    global currentDeaths
+    global currentRound
+    global gameRound
+
+    keyChar = ''
+    try:
+        keyChar = format(key.char)
+    except AttributeError:
+        foo = 0
+    except:
+        print("Es gab nen fehler beim Taste einlesen - Nichts worüber man sich sorgen machen sollte")
+    
+    if keyChar == 'i':
+        pause = not pause
+        print ("Pause ist "+str(pause))
+    if keyChar == 'o':
+        deaths = 0
+        currentDeaths = 0
+        currentRound = 0
+        gameRound = 0
+        print ("RESET")
+
+
+def on_release(key):
+    if key == keyboard.Key.page_down:
+        # Stop listener
+        os._exit(1)
+        return False
+
+def startKeyListener():
+    with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+        listener.join()
+
+
+def mute():
+    keyboardOut = keyboard.Controller()
+
+    keyboardOut.press('k')
+    keyboardOut.press('l')
+    keyboardOut.press('n')
+    keyboardOut.release('k')
+    keyboardOut.release('l')
+    keyboardOut.release('n')
+
+
+def unMute():
+    keyboardOut = keyboard.Controller()
+
+    keyboardOut.press('p')
+    keyboardOut.press('n')
+    keyboardOut.press('m')
+    keyboardOut.release('p')
+    keyboardOut.release('n')
+    keyboardOut.release('m')
 
 
 if __name__ == '__main__':
+    thread = Thread(target=startKeyListener, args=())
+    thread.start()
     app.run(host='127.0.0.1', port=3000, debug=True)
     #app.run(host='0.0.0.0', port=5000)
